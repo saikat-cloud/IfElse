d35b1077b153f7e0f663bf5c5025d9c1d7214eb9
saikat <saikat.mondal.dev@gmail.com>
2023-02-28 10:35:51 +0600

    and or not, if else.

---
+++ AndOrNot.py
@@ -0,0 +1,50 @@
+
+
+
+a = 200
+b = 33
+c = 500
+
+if a > b and c > a:
+  print("Both conditions are True")
+
+
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+
+a = 100
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+
+
+x = 41
+
+if x > 10:
+  print("Above 10,")
+  if x > 30:
+    print("and also above 30!")
+  else:
+    print("but not above 20.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ IfElse.py
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+a = 60
+b = 60
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+        #One line if else statement
+print("A") if a > b else print("B")
+        #One line if else statement, with 3 conditions
+print("A") if a > b else print("=") if a == b else print("B")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = input("Enter First Number :")
+# b = input("enter Secend Numaber :")
+
+# if a > b :
+#     print("a is greater than b" + a > b)
+# elif a < b:
+#     print("a is less then b" + a < b)
+# else:
+#     print("a and b are equal" + a == b)
+
+
+
+
+
